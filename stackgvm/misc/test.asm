be42ca8457893413f3e942980888c41c23f45bb7
kchurchill <karl.churchill@gear4music.com>
2019-01-14 19:41:07 +0000

    Messing around with CPP as a preprocessor

--- stackgvm/misc/test.asm
+++ stackgvm/misc/test.asm
@@ -1,12 +1,21 @@
 
-@aimProjectile(Actor*,Actor*,float):vector
-    move   4, i0           ; load target ref into indirection register 0
-    move   3, i1           ; load attacker ref into indirection register 1
-    vsub   0(i0), 0(i1), 0 ; direction = target pos - attacker pos
-    vmag   0, 10           ; distance = _magnitude(direction)
-    fdiv   10, 5, 10       ; flightTime = distance / speed
-    vscl   9(i0), 10, 0    ; direction = target velocity * flightTime
-    vadd   0(i0), 0, 0     ; direction += target position
-    vsub   0, 0(i1), 0     ; direction -= attacker position
-    vnrm   0, 0            ; result = _normalize(direction)
+#include "actor.inc"
+
+#define r_actor (3)
+#define f_time  (4)
+#define v_result (0)
+#define f_time2 (5)
+#define f_half_time2 (6)
+#define v_temp (7)
+
+@predictPosition(Actor* actor, float time):vector
+    load   r_actor, i0
+    mulf.v (i0 + v_actor_velocity), f_time, v_result
+    loadc  F_ONE_HALF, f_half_time2
+    mul.f  f_time, f_time, f_time2
+    mul.f  f_half_time2, f_time2, f_half_time2
+    mulf.v (i0 + v_actor_acceleration), f_half_time2, v_temp
+    add.v  v_temp, v_result, v_result
+    add.v  (i0 + v_actor_position), v_result, v_result
     ret
+
