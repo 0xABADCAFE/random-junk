9f17acf5f997c37aae6cd5eb98736427a74da6b6
kchurchill <karl.churchill@gear4music.com>
2019-02-07 19:00:18 +0000

    Implemented simple project file format and replaced directory scanning model.

--- stackgvm/assembler/asm.php
+++ stackgvm/assembler/asm.php
@@ -14,46 +14,96 @@ $aOptions = getopt(
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Project {
+    private $oProjectData;
 
     public function __construct(string $sProjectFile) {
-
+        $this->oProjectData = $this->load($sProjectFile);
     }
 
-    public function get_source_directory() : string {
-        return $this->sSourceDirectory;
+    public function getName() : string {
+        return $this->oProjectData->name;
     }
 
-    public function get_include_paths() : array {
-        return $this->aIncludePaths;
+    public function getBasePath() : string {
+        return $this->oProjectData->base;
+    }
+
+    public function getSources() : array {
+        return $this->oProjectData->sources;
+    }
+
+    public function getIncludePath() : string {
+        return $this->oProjectData->incdir;
+    }
+
+    public function getTargetName() : string {
+        return $this->oProjectData->host->name;
+    }
+
+    public function prepare() {
+        $aFiles = array_map(
+            function(string $sPath) {
+                $sPath = $this->oProjectData->base . $sPath;
+                if (
+                    !file_exists($sPath) ||
+                    !is_readable($sPath)
+                ) {
+                    throw new Exception("Invalid source path '" . $sPath . "'");
+                }
+                return $sPath;
+            },
+            $this->oProjectData->sources
+        );
+        $sIncludeDir = $this->oProjectData->base . $this->oProjectData->incdir;
+        if (
+            !is_dir($sIncludeDir) ||
+            !is_readable($sIncludeDir)
+        ) {
+            throw new Exception("Invalid include path '" . $sIncludeDir . "'");
+        }
+
+        $this->oProjectData->sources = $aFiles;
+        $this->oProjectData->incdir  = $sIncludeDir;
+    }
+
+    private function load(string $sProjectFile) {
+        if (
+            !file_exists($sProjectFile) ||
+            !is_readable($sProjectFile)
+        ) {
+            throw new Exception("Unable to open project file '" . $sProjectFile . "' for reading.\n");
+        }
+        $oProjectData = json_decode(file_get_contents($sProjectFile));
+        if (
+            !($oProjectData instanceof stdClass) ||
+            !isset($oProjectData->name) ||
+            !is_array($oProjectData->sources) ||
+            !isset($oProjectData->incdir) ||
+            !isset($oProjectData->host->name)
+        ) {
+            throw new Exception("Invalid project file '" . $sProjectFile . "'");
+        }
+        $oProjectData->base = realpath(dirname($sProjectFile)) .'/';
+        return $oProjectData;
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class SourceLoader {
 
-    private $aSource = [];
+    private $oProject = null;
+    private $aSource  = [];
 
-    public function __construct(string $sSourceDirectory, $sIncludeDirectory = null) {
-        $this->validateDirectory($sSourceDirectory, 'source');
-        $this->sSourceDirectory = $sSourceDirectory;
-        if (null === $sIncludeDirectory) {
-            $this->sIncludeDirectory = $sIncludeDirectory;
-        } else {
-            $this->validateDirectory($sIncludeDirectory, 'include');
-            $this->sIncludeDirectory = $sIncludeDirectory;
-        }
-
+    public function __construct(Project $oProject) {
+        $this->oProject = $oProject;
     }
 
     public function load() : self {
-        echo "Processing files in ", $this->sSourceDirectory, "\n";
-        $oSourceDirectory = dir($this->sSourceDirectory);
-        while (false !== ($sFileName = $oSourceDirectory->read())) {
-            $sCurrentPath = $this->sSourceDirectory . '/' . $sFileName;
-            if ($this->checkFile($sCurrentPath)) {
-                $this->preprocessFile($sCurrentPath);
-            }
+        $this->oProject->prepare();
+        foreach ( $this->oProject->getSources() as $sCurrentPath) {
+            $this->preprocessFile($sCurrentPath);
         }
-        $oSourceDirectory->close();
         return $this;
     }
 
@@ -61,30 +111,11 @@ class SourceLoader {
         print_r($this->aSource);
     }
 
-    private function checkFile(string $sCurrentPath) : bool {
-        if (is_link($sCurrentPath) || is_dir($sCurrentPath)) {
-            return false;
-        }
-        if (strtolower(substr($sCurrentPath, -4, 4)) === '.asm') {
-            if (!is_readable($sCurrentPath)) {
-                throw new Exception("Unable to open ", $sCurrentPath, " for reading");
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private function validateDirectory(string $sDirectory, string $sType) {
-        if (
-            !is_dir($sDirectory) ||
-            !is_readable($sDirectory)
-        ) {
-            throw new Exception('Invalid ' . $sType . ' directory path');
-        }
-    }
-
     private function preprocessFile(string $sCurrentPath) {
-        $sContent = shell_exec('cpp -nostdinc -I' . $this->sIncludeDirectory . ' -Isdk/test_host/include ' . $sCurrentPath);
+        $sContent = shell_exec(
+            'cpp -nostdinc -I' . $this->oProject->getIncludePath() .
+            ' -Isdk/' . $this->oProject->getTargetName() . '/include ' .
+            $sCurrentPath);
         $this->aSource[$sCurrentPath] = preg_replace('/^#.*?$\n*/m', '', $sContent);
     }
 
@@ -94,7 +125,7 @@ class SourceLoader {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-(new SourceLoader('example', 'example/include'))
+
+(new SourceLoader(new Project('example/project.json')))
     ->load()
     ->dump();
-
