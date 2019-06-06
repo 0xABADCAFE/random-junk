784e96aecd206adc1185eff48fed3babc0aea0b1
Hamza <karlchurchill@gmail.com>
2019-06-06 17:44:54 +0100

    Added not equal comparison

---
+++ stackgvm/source/include/gvm_branch/bits_not_equal.hpp
@@ -0,0 +1,147 @@
+
+// Local !== Small Integer Literal
+BCC(X_NE_LS) {
+    gvmDebugOpcode(
+        "bcc.i eq (%d), #%d, %d : %d != %d => ",
+        OPS(1),
+        OPS(2),
+        (int)J16(3),
+        LOC(1).i,
+        S8(2)
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (LOC(1).i != S8(2)) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Indirect[0] !== Small Integer Literal
+BCC(X_NE_I0S) {
+    gvmDebugOpcode(
+        "bcc.i eq (i0 + %u), #%d, %d : %d != %d => ",
+        OPU(1),
+        OPS(2),
+        (int)J16(3),
+        IX0(1).i,
+        S8(2)
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (IX0(1).i != S8(2)) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Indirect[1] !== Small Integer Literal
+BCC(X_NE_I1S) {
+    gvmDebugOpcode(
+        "bcc.i eq (i1 + %u), #%d, %d : %d != %d => ",
+        OPU(1),
+        OPS(2),
+        (int)J16(3),
+        IX1(1).i,
+        S8(2)
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (IX1(1).i != S8(2)) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Local !== Local
+BCC(X_NE_LL) {
+    gvmDebugOpcode(
+        "bcc eq (%d), (%d), %d : 0x%08X != 0x%08X => ",
+        OPS(1),
+        OPS(2),
+        (int)J16(3),
+        LOC(1).u,
+        LOC(2).u
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (LOC(1).u != LOC(2).u) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Local !== Indirect[0]
+BCC(X_NE_LI0) {
+    gvmDebugOpcode(
+        "bcc eq (%d), (i0 + %u), %d : 0x%08X != 0x%08X => ",
+        OPS(1),
+        OPU(2),
+        (int)J16(3),
+        LOC(1).u,
+        IX0(2).u
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (LOC(1).u != IX0(2).u) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Local !== Indirect[1]
+BCC(X_NE_LI0) {
+    gvmDebugOpcode(
+        "bcc eq (%d), (i1 + %u), %d : 0x%08X != 0x%08X => ",
+        OPS(1),
+        OPU(2),
+        (int)J16(3),
+        LOC(1).u,
+        IX1(2).u
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (LOC(1).u != IX1(2).u) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Indirect[0] !== Indirect[1]
+BCC(X_NE_LII) {
+    gvmDebugOpcode(
+        "bcc eq (i0 + %u), (i1 + %u), %d : 0x%08X != 0x%08X => ",
+        OPU(1),
+        OPU(2),
+        (int)J16(3),
+        IX0(1).u,
+        IX1(2).u
+    );
+    // Branch to a signed 16-bit offset if two local values are not equal
+    if (IX0(1).u != IX1(2).u) {
+        gvmDebugJump(2);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
