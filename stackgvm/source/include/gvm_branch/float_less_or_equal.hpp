7ef195d191d8c4c6b74674a6b58653414b21ae34
kchurchill <karl.churchill@gear4music.com>
2019-06-07 19:07:12 +0100

    Implementing floating point BOC operations.

---
+++ stackgvm/source/include/gvm_branch/float_less_or_equal.hpp
@@ -0,0 +1,80 @@
+
+// Local <= Local
+BCC(F_LE_LL) {
+    gvmDebugOpcode(
+        "bcc le.f (%d), (%d), %d : %e <= %e => ",
+        OPS(1),
+        OPS(2),
+        (int)J16(3),
+        LOC(1).f,
+        LOC(2).f
+    );
+    if (LOC(1).f <= LOC(2).f) {
+        gvmDebugJump(3);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Local <= Indirect[0]
+BCC(F_LE_LI0) {
+    gvmDebugOpcode(
+        "bcc le.f (%d), (i0 + %u), %d : %e <= %e => ",
+        OPS(1),
+        OPU(2),
+        (int)J16(3),
+        LOC(1).f,
+        IX0(2).f
+    );
+    if (LOC(1).f <= IX0(2).f) {
+        gvmDebugJump(3);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Local <= Indirect[1]
+BCC(F_LE_LI1) {
+    gvmDebugOpcode(
+        "bcc le.f (%d), (i1 + %u), %d : %e <= %e => ",
+        OPS(1),
+        OPU(2),
+        (int)J16(3),
+        LOC(1).f,
+        IX1(2).f
+    );
+    if (LOC(1).f <= IX1(2).f) {
+        gvmDebugJump(3);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
+
+// Indirect[0] <= Indirect[1]
+BCC(F_LE_II) {
+    gvmDebugOpcode(
+        "bcc le.f (i0 + %u), (i1 + %u), %d : %e <= %e => ",
+        OPU(1),
+        OPU(2),
+        (int)J16(3),
+        IX0(1).f,
+        IX1(2).f
+    );
+    if (IX0(1).f <= IX1(2).f) {
+        gvmDebugJump(3);
+        STEP(J16(3));
+        NEXT;
+    }
+    gvmDebugSkip();
+    STEP(6);
+    NEXT;
+}
--- stackgvm/source/include/gvm_conditional_branch.hpp
+++ stackgvm/source/include/gvm_conditional_branch.hpp
@@ -12,11 +12,12 @@ IS(BOC) {
         #include "gvm_branch/int_greater_than.hpp"
         #include "gvm_branch/bit_set.hpp"
         #include "gvm_branch/bit_not_set.hpp"
-/*
         #include "gvm_branch/float_less_than.hpp"
+
         #include "gvm_branch/float_less_or_equal.hpp"
         #include "gvm_branch/float_greater_or_equal.hpp"
         #include "gvm_branch/float_greater_than.hpp"
+        /*
         #include "gvm_branch/float_approx_equal.hpp"
         #include "gvm_branch/vector_equal.hpp"
         #include "gvm_branch/vector_approx_equal.hpp"
