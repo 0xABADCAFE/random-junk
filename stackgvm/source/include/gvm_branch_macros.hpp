978b2fb5d3ec2ce7081e70c21cb7ca3c28375d2e
kchurchill <karl.churchill@gear4music.com>
2019-06-11 20:02:39 +0100

    Adding the argument reversed, logic reversed "continue if indirect <operation> local" is a bit head melty.

--- stackgvm/source/include/gvm_branch_macros.hpp
+++ stackgvm/source/include/gvm_branch_macros.hpp
@@ -40,15 +40,16 @@
 #define blt_li (l, I, i, j) _CC(I_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define blt_ii (i1, i2, j)  _CC(I_LT_II),      _D8(i1), _D8(i2), _D16(j),
 
-#define blt_il (i, l, j)    _CC(I_GE_LI),      _D8(l),  _D8(i),  _D16(j),
+#define blt_il (I, i, l, j) _CC(I_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Integer continue if less than (alias)
 #define clt_ls (l, s, j)    _CC(I_GE_LS),      _D8(l),  _D8(s),  _D16(j),
 #define clt_is (I, i, s, j) _CC(I_GE_I##I##S), _D8(i),  _D8(s),  _D16(j),
 #define clt_ll (l1, l2, j)  _CC(I_GE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define clt_li (l, I, i, j) _CC(I_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define clt_ii (i1, i2, j)  _CC(I_GE_II),     _D8(i1), _D8(i2), _D16(j),
+#define clt_ii (i1, i2, j)  _CC(I_GE_II),      _D8(i1), _D8(i2), _D16(j),
 
+#define clt_il (I, i, l, j) _CC(I_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Integer branch if less than or equal
 #define ble_ls (l, s, j)    _CC(I_LE_LS),      _D8(l),  _D8(s),  _D16(j),
@@ -57,13 +58,17 @@
 #define ble_li (l, I, i, j) _CC(I_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define ble_ii (i1, i2, j)  _CC(I_LE_II),     _D8(i1), _D8(i2), _D16(j),
 
+#define ble_il (I, i, l, j) _CC(I_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
+
 // Integer continue if less than or equal (alias)
 #define cle_ls (l, s, j)    _CC(I_GT_LS),      _D8(l),  _D8(s),  _D16(j),
 #define cle_is (I, i, s, j) _CC(I_GT_I##I##S), _D8(i),  _D8(s),  _D16(j),
 #define cle_ll (l1, l2, j)  _CC(I_GT_LL),      _D8(l1), _D8(l2), _D16(j),
 #define cle_li (l, I, i, j) _CC(I_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define cle_ii (i1, i2, j)  _CC(I_GT_II),     _D8(i1), _D8(i2), _D16(j),
+#define cle_ii (i1, i2, j)  _CC(I_GT_II),      _D8(i1), _D8(i2), _D16(j),
 
+// TODO - correct?
+#define cle_il (I, i, l, j) _CC(I_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Integer branch if greater than or equal
 #define bge_ls (l, s, j)    _CC(I_GE_LS),      _D8(l),  _D8(s),  _D16(j),
@@ -72,6 +77,8 @@
 #define bge_li (l, I, i, j) _CC(I_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define bge_ii (i1, i2, j)  _CC(I_GE_II),     _D8(i1), _D8(i2), _D16(j),
 
+#define bge_il (I, i, l, j) _CC(I_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
+
 // Integer continue if greater than or equal (alias)
 #define cge_ls (l, s, j)    _CC(I_LT_LS),      _D8(l),  _D8(s),  _D16(j),
 #define cge_is (I, i, s, j) _CC(I_LT_I##I##S), _D8(i),  _D8(s),  _D16(j),
@@ -79,6 +86,8 @@
 #define cge_li (l, I, i, j) _CC(I_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define cge_ii (i1, i2, j)  _CC(I_LT_II),     _D8(i1), _D8(i2), _D16(j),
 
+// TODO - correct?
+#define cge_il (I, i, l, j) _CC(I_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Integer branch if greater than
 #define bgt_ls (l, s, j)    _CC(I_GT_LS),      _D8(l),  _D8(s),  _D16(j),
@@ -87,6 +96,8 @@
 #define bgt_li (l, I, i, j) _CC(I_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define bgt_ii (i1, i2, j)  _CC(I_GT_II),     _D8(i1), _D8(i2), _D16(j),
 
+#define bgt_il (I, i, l, j) _CC(I_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
+
 // Integer continue if greater than (alias)
 #define cgt_ls (l, s, j)    _CC(I_LE_LS),      _D8(l),  _D8(s),  _D16(j),
 #define cgt_is (I, i, s, j) _CC(I_LE_I##I##S), _D8(i),  _D8(s),  _D16(j),
@@ -94,6 +105,8 @@
 #define cgt_li (l, I, i, j) _CC(I_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 #define cgt_ii (i1, i2, j)  _CC(I_LE_II),     _D8(i1), _D8(i2), _D16(j),
 
+// TODO - correct?
+#define cgt_il (I, i, l, j) _CC(I_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Untyped branch if bit is set
 #define bbs_sl (s, l, j)    _CC(X_BS_SL),      _D8(s),  _D8(l),  _D16(j),
@@ -124,70 +137,78 @@
 // Float branch if less than
 #define fblt_ll (l1, l2, j)  _CC(F_LT_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fblt_li (l, I, i, j) _CC(F_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fblt_ii (i1, i2, j)  _CC(F_LT_II),     _D8(i1), _D8(i2), _D16(j),
+#define fblt_ii (i1, i2, j)  _CC(F_LT_II),      _D8(i1), _D8(i2), _D16(j),
+
+#define fblt_il (I, i, l, j) _CC(F_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Float continue if less than (alias)
 #define fclt_ll (l1, l2, j)  _CC(F_GE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fclt_li (l, I, i, j) _CC(F_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fclt_ii (i1, i2, j)  _CC(F_GE_II),     _D8(i1), _D8(i2), _D16(j),
+#define fclt_ii (i1, i2, j)  _CC(F_GE_II),      _D8(i1), _D8(i2), _D16(j),
 
 
 // Float branch if less than or equal
 #define fble_ll (l1, l2, j)  _CC(F_LE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fble_li (l, I, i, j) _CC(F_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fble_ii (i1, i2, j)  _CC(F_LE_II),     _D8(i1), _D8(i2), _D16(j),
+#define fble_ii (i1, i2, j)  _CC(F_LE_II),      _D8(i1), _D8(i2), _D16(j),
+
+#define fble_il (I, i, l, j) _CC(F_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Float contimue if less than or equal (alias)
 #define fcle_ll (l1, l2, j)  _CC(F_GT_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fcle_li (l, I, i, j) _CC(F_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fcle_ii (i1, i2, j)  _CC(F_GT_II),     _D8(i1), _D8(i2), _D16(j),
+#define fcle_ii (i1, i2, j)  _CC(F_GT_II),      _D8(i1), _D8(i2), _D16(j),
 
 
 // Float branch if greater than or equal
 #define fbge_ll (l1, l2, j)  _CC(F_GE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fbge_li (l, I, i, j) _CC(F_GE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fbge_ii (i1, i2, j)  _CC(F_GE_II),     _D8(i1), _D8(i2), _D16(j),
+#define fbge_ii (i1, i2, j)  _CC(F_GE_II),      _D8(i1), _D8(i2), _D16(j),
+
+#define fbge_il (I, i, l, j) _CC(F_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Float continue if greater than or equal (alias)
 #define fcge_ll (l1, l2, j)  _CC(F_LT_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fcge_li (l, I, i, j) _CC(F_LT_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fcge_ii (i1, i2, j)  _CC(F_LT_II),     _D8(i1), _D8(i2), _D16(j),
+#define fcge_ii (i1, i2, j)  _CC(F_LT_II),      _D8(i1), _D8(i2), _D16(j),
 
 
 // Float branch if greater than
 #define fbgt_ll (l1, l2, j)  _CC(F_GT_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fbgt_li (l, I, i, j) _CC(F_GT_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fbgt_ii (i1, i2, j)  _CC(F_GT_II),     _D8(i1), _D8(i2), _D16(j),
+#define fbgt_ii (i1, i2, j)  _CC(F_GT_II),      _D8(i1), _D8(i2), _D16(j),
+
+#define fbgt_il (I, i, l, j) _CC(F_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
 
 // Float continue if greater than (alias)
 #define fcgt_ll (l1, l2, j)  _CC(F_LE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fcgt_li (l, I, i, j) _CC(F_LE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fcgt_ii (i1, i2, j)  _CC(F_LE_II),     _D8(i1), _D8(i2), _D16(j),
+#define fcgt_ii (i1, i2, j)  _CC(F_LE_II),      _D8(i1), _D8(i2), _D16(j),
 
 // Float branch if almost equal
 #define fbae_ll (l1, l2, j)  _CC(F_AE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define fbae_li (l, I, i, j) _CC(F_AE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define fbae_ii (i1, i2, j)  _CC(F_AE_II),     _D8(i1), _D8(i2), _D16(j),
+#define fbae_ii (i1, i2, j)  _CC(F_AE_II),      _D8(i1), _D8(i2), _D16(j),
 
 
 // Vector branch if equal
 #define vbeq_ll (l1, l2, j)  _CC(V_EQ_LL),      _D8(l1), _D8(l2), _D16(j),
 #define vbeq_li (l, I, i, j) _CC(V_EQ_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define vbeq_ii (i1, i2, j)  _CC(V_EQ_II),     _D8(i1), _D8(i2), _D16(j),
+#define vbeq_ii (i1, i2, j)  _CC(V_EQ_II),      _D8(i1), _D8(i2), _D16(j),
 
 // Vector continue if equal (alias)
 #define vceq_ll (l1, l2, j)  _CC(V_NE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define vceq_li (l, I, i, j) _CC(V_NE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define vceq_ii (i1, i2, j)  _CC(V_NE_II),     _D8(i1), _D8(i2), _D16(j),
+#define vceq_ii (i1, i2, j)  _CC(V_NE_II),      _D8(i1), _D8(i2), _D16(j),
 
 // Vector branch if almost equal
 #define vbae_ll (l1, l2, j)  _CC(V_AE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define vbae_li (l, I, i, j) _CC(V_AE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define vbae_ii (i1, i2, j)  _CC(V_AE_II),     _D8(i1), _D8(i2), _D16(j),
+#define vbae_ii (i1, i2, j)  _CC(V_AE_II),      _D8(i1), _D8(i2), _D16(j),
 
 // Vector branch if not equal
 #define vbne_ll (l1, l2, j)  _CC(V_NE_LL),      _D8(l1), _D8(l2), _D16(j),
 #define vbne_li (l, I, i, j) _CC(V_NE_LI##I),   _D8(l),  _D8(i),  _D16(j),
-#define vbne_ii (i1, i2, j)  _CC(V_NE_II),     _D8(i1), _D8(i2), _D16(j),
+#define vbne_ii (i1, i2, j)  _CC(V_NE_II),      _D8(i1), _D8(i2), _D16(j),
 
 #endif
