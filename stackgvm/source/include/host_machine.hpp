1dda812ffafe4470b8d42e8610cfbc1831908858
Hamza <karlchurchill@gmail.com>
2019-06-15 14:42:18 +0100

    Starting 64-bit x64 host port.

--- stackgvm/source/include/host_machine.hpp
+++ stackgvm/source/include/host_machine.hpp
@@ -7,9 +7,6 @@
 #ifndef _GVM_HOST_MACHINE_HPP_
     #define _GVM_HOST_MACHINE_HPP_
     #include <cstdlib>
-    #ifdef __LP64__
-        #error 64-bit targets are not currently supported
-    #endif
 
     #define _GVM_HOST_BIG    0
     #define _GVM_HOST_LITTLE 1
@@ -20,6 +17,7 @@
     #define _GVM_HOST_AMIGAOS4_PPC     3
     #define _GVM_HOST_MORPHOS_PPC      4
     #define _GVM_HOST_LINUX_INTEL_32   5
+    #define _GVM_HOST_LINUX_INTEL_64   6
 
     // Target sanity checks
     #if (_GVM_HOST_OS == _GVM_HOST_AMIGAOS3_68K)
@@ -30,6 +28,37 @@
         #endif
     #endif
 
+    #if (_GVM_HOST_OS == _GVM_HOST_LINUX_INTEL_32)
+        #ifdef __LP64__
+            #warning 32-bit directive set for x64 target, switching to x64
+            #undef _GVM_HOST_OS
+            #define _GVM_HOST_OS _GVM_HOST_LINUX_INTEL_64
+        #endif
+    #endif
+
+    #ifdef __LP64__
+    // 64-bit typedefs
+    typedef signed char int8;
+    typedef signed short int int16;
+    typedef signed int int32;
+    typedef signed long int int64;
+    typedef unsigned char uint8;
+    typedef unsigned short int uint16;
+    typedef unsigned int uint32;
+    typedef unsigned long int uint64;
+    typedef float float32;
+    typedef double float64;
+
+    // Formatting Templates
+    #define FS16 "hd"
+    #define FU16 "hu"
+    #define FS32 "d"
+    #define FU32 "u"
+    #define FS64 "ld"
+    #define FU64 "lu"
+
+    #else
+    // 32-bit typedefs
     typedef signed char int8;
     typedef signed short int int16;
     typedef signed long int int32;
@@ -49,8 +78,13 @@
     #define FS64 "lld"
     #define FU64 "llu"
 
+    #endif
+
     // target specific class implementations
     #if (_GVM_HOST_OS == _GVM_HOST_LINUX_INTEL_32)
+        #define _GVM_HOST_ENDIAN _GVM_HOST_LITTLE
+        #include "platforms/machine_linux_generic.hpp"
+    #if (_GVM_HOST_OS == _GVM_HOST_LINUX_INTEL_64)
         #define _GVM_HOST_ENDIAN _GVM_HOST_LITTLE
         #include "platforms/machine_linux_generic.hpp"
     //#elif (_GVM_HOST_OS == _GVM_HOST_AMIGAOS3_68K)
