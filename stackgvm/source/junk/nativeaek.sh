a3b56a9a1addcc185cdfdad07f0ad36d2ef4fe26
kchurchill <karl.churchill@gear4music.com>
2019-06-27 17:43:25 +0100

    Stuff

--- stackgvm/source/junk/nativeaek.sh
+++ stackgvm/source/junk/nativeaek.sh
@@ -14,12 +14,12 @@ g++ -Ofast -Wall -flto -W -m32 -march=native -funroll-loops -mfpmath=sse -fomit-
 g++ -Ofast -Wall -flto -W -m32 -march=native -funroll-loops -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_2.cpp -o bin/aek2_x86_sse &
 g++ -Ofast -Wall -flto -W -m32 -march=native -funroll-loops -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_3.cpp -o bin/aek3_x86_sse
 
-g++ -Ofast -Wall -flto -W -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek.cpp -o bin/aek_x64_avx &
-g++ -Ofast -Wall -flto -W -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_2.cpp -o bin/aek2_x64_avx &
-g++ -Ofast -Wall -flto -W -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_3.cpp -o bin/aek3_x64_avx &
-g++ -Ofast -Wall -flto -W -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek.cpp -o bin/aek_x86_avx &
-g++ -Ofast -Wall -flto -W -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_2.cpp -o bin/aek2_x86_avx &
-g++ -Ofast -Wall -flto -W -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_3.cpp -o bin/aek3_x86_avx
+g++ -Ofast -Wall -flto -W -ftree-vectorize -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek.cpp -o bin/aek_x64_avx &
+g++ -Ofast -Wall -flto -W -ftree-vectorize -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_2.cpp -o bin/aek2_x64_avx &
+g++ -Ofast -Wall -flto -W -ftree-vectorize -march=native -mavx      -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_3.cpp -o bin/aek3_x64_avx &
+g++ -Ofast -Wall -flto -W -ftree-vectorize -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek.cpp -o bin/aek_x86_avx &
+g++ -Ofast -Wall -flto -W -ftree-vectorize -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_2.cpp -o bin/aek2_x86_avx &
+g++ -Ofast -Wall -flto -W -ftree-vectorize -m32 -march=native -mavx -funroll-loops -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations aek_3.cpp -o bin/aek3_x86_avx
 
 #time ./aek > /dev/null
 #time ./aek2 > /dev/null
