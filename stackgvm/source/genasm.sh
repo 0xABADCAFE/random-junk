8e51634fabf857d26b8c18a3815221615b452ed2
kchurchill <karl.churchill@gear4music.com>
2019-08-07 20:19:20 +0100

    Introspection of assembler output for address generation.

--- stackgvm/source/genasm.sh
+++ stackgvm/source/genasm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 g++ -Ofast -Wall -W -m32 -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_32 -D_GVM_ANNOTATE_ASM_ -S gvm_run.cpp -o bin/gvm_run_x86.asm &
 g++ -Ofast -Wall -W -m32 -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_32 -D_GVM_ANNOTATE_ASM_ -S gvm_address.cpp -o bin/gvm_address_x86.asm &
+g++ -Ofast -Wall -W -fPIC -m32 -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_32 -D_GVM_ANNOTATE_ASM_ -S gvm_address.cpp -o bin/gvm_address_PIC_x86.asm &
 
 g++ -Ofast -Wall -W -m32 -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_32 -D_GVM_OPT_ALLOW_GOTO_ -D_GVM_ANNOTATE_ASM_ -S gvm_run.cpp -o bin/gvm_run_allow_goto_x86.asm &
 
@@ -11,6 +12,7 @@ g++ -Ofast -Wall -W -fPIC -m32 -march=native -mfpmath=sse -fomit-frame-pointer -
 
 g++ -Ofast -Wall -W -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_64 -D_GVM_OPT_64BIT_PURE_ -D_GVM_ANNOTATE_ASM_ -S gvm_run.cpp -o bin/gvm_run_x64.asm &
 g++ -Ofast -Wall -W -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_64 -D_GVM_OPT_64BIT_PURE_ -D_GVM_ANNOTATE_ASM_ -S gvm_address.cpp -o bin/gvm_address_x64.asm &
+g++ -Ofast -Wall -W -fPIC -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_64 -D_GVM_OPT_64BIT_PURE_ -D_GVM_ANNOTATE_ASM_ -S gvm_address.cpp -o bin/gvm_address_PIC_x64.asm &
 
 g++ -Ofast -Wall -W -march=native -mfpmath=sse -fomit-frame-pointer -fno-exceptions -fexpensive-optimizations -D_GVM_HOST_OS=_GVM_HOST_LINUX_INTEL_64 -D_GVM_OPT_64BIT_PURE_ -D_GVM_OPT_ALLOW_GOTO_ -D_GVM_ANNOTATE_ASM_ -S gvm_run.cpp -o bin/gvm_run_allow_goto_x64.asm &
 
